c4ae15062b56d1ac3c3177c7d4ecdb8b0d99971f
host4ideas <72515482+host4ideas@users.noreply.github.com>
2023-01-11 09:59:58 +0100

    Form and button actions

--- csharp/Fundamentos/Fundamentos/Form1.Designer.cs
+++ csharp/Fundamentos/Fundamentos/Form1.Designer.cs
@@ -28,32 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnPulsar = new System.Windows.Forms.Button();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btnPulsar
             // 
-            this.button1.Location = new System.Drawing.Point(295, 125);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 51);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnPulsar.BackColor = System.Drawing.Color.Coral;
+            this.btnPulsar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnPulsar.Location = new System.Drawing.Point(245, 187);
+            this.btnPulsar.Name = "btnPulsar";
+            this.btnPulsar.Size = new System.Drawing.Size(197, 73);
+            this.btnPulsar.TabIndex = 0;
+            this.btnPulsar.Text = "Pulsar";
+            this.btnPulsar.UseVisualStyleBackColor = false;
+            this.btnPulsar.Click += new System.EventHandler(this.btnPulsar_Click);
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelNombre.Location = new System.Drawing.Point(172, 100);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(115, 36);
+            this.labelNombre.TabIndex = 1;
+            this.labelNombre.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNombre.Location = new System.Drawing.Point(358, 100);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 41);
+            this.txtNombre.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Formulario";
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.btnPulsar);
+            this.Name = "Form1";
             this.Text = "Formulario";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Button button1;
+        private Button btnPulsar;
+        private Label labelNombre;
+        private TextBox txtNombre;
     }
 }
--- csharp/Fundamentos/Fundamentos/Form1.cs
+++ csharp/Fundamentos/Fundamentos/Form1.cs
@@ -6,5 +6,21 @@ namespace Fundamentos
         {
             InitializeComponent();
         }
+
+        private void btnPulsar_Click(object sender, EventArgs e)
+        {
+            // Aquí es donde esbribiremos nuestras acciones al pulsar el botón
+            this.txtNombre.Width= 150;
+            this.btnPulsar.Text = "Botón pulsado!!!";
+            Random rnd = new();
+            int x = rnd.Next(600);
+            int y = rnd.Next(600);
+            this.txtNombre.Location = new Point(x, y);
+            this.txtNombre.TextAlign = HorizontalAlignment.Center;
+            int r = rnd.Next(256);
+            int g = rnd.Next(256);
+            int b = rnd.Next(256);
+            this.BackColor = Color.FromArgb(r, g, b);
+        }
     }
 }
