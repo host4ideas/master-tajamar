a8d381b9ff78969e8332db36463596a10827cb3b
host4ideas <72515482+host4ideas@users.noreply.github.com>
2023-02-02 12:30:14 +0100

    first mvc

---
+++ dotnet/PrimerMvcNetCore/PrimerMvcNetCore/Controllers/PruebaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class PruebaController : Controller
+    {
+
+        public IActionResult PrimeraPagina()
+        {
+            return View();
+        }
+    }
+}
