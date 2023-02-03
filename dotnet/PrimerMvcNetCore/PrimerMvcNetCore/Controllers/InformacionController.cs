73d1ee28e298cbab77e447619e612d954b1e578e
host4ideas <72515482+host4ideas@users.noreply.github.com>
2023-02-03 14:22:19 +0100

    comienzo MVC

---
+++ dotnet/PrimerMvcNetCore/PrimerMvcNetCore/Controllers/InformacionController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerMvcNetCore.Models;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(string variable1, string variable2)
+        {
+            return View();
+        }
+
+        public IActionResult VistaControladorGet
+            (
+            string app, Nullable<int> version
+            )
+        {
+            if (version is null)
+            {
+                ViewData["DATOS"] = "Solo Application: " + app;
+            }
+            else
+            {
+                ViewData["DATOS"] = "Application: " + app + ", Version: " + version.GetValueOrDefault();
+            }
+            return View();
+        }
+
+        public IActionResult VistaControladorPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult VistaControladorPost
+        (
+        //string cajaNombre, string cajaEmail, int cajaEdad
+        Persona persona, int inventado
+        )
+        {
+            //ViewData["DATOS"] = "Nombre: " + cajaNombre + ", Email: " + cajaEmail + ", Edad: " + cajaEdad;
+            ViewData["DATOS"] = "Nombre: " + persona.Nombre +
+                ", Email: " + persona.Email +
+                ", Edad: " + persona.Edad +
+                ", Inventado: " + inventado;
+            return View();
+        }
+
+        public IActionResult ControladorVista()
+        {
+            Persona persona = new()
+            {
+                Nombre = "Persona Model",
+                Edad = 23,
+                Email = "example@example.com"
+            };
+
+            ViewData["Nombre"] = "Felix";
+            ViewData["Edad"] = 23;
+
+            return View(persona);
+        }
+    }
+}
---
+++ dotnet/PrimerMvcNetCore/PrimerMvcNetCore/Controllers/MatematicasController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class MatematicasController : Controller
+    {
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        public IActionResult SumarNumerosGet(int num1, int num2)
+        {
+            ViewData["RESULTADO"] = num1 + num2;
+            return View();
+        }
+
+        public IActionResult SumarNumerosPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SumarNumerosPost(int num1, int num2)
+        {
+            ViewData["RESULTADO"] = num1 + num2;
+            return View();
+        }
+    }
+}
