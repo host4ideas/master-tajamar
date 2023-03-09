32c4ae2f28753070a1270f6db6804ac8483d6bc5
host4ideas <72515482+host4ideas@users.noreply.github.com>
2023-03-09 10:43:49 +0100

    mvc utilidades

---
+++ dotnet/MvcCoreUtilidades/MvcCoreUtilidades/Controllers/CachingController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace MvcCryptography.Controllers
+{
+    public class CachingController : Controller
+    {
+        IMemoryCache memoryCache;
+
+        public CachingController(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        public IActionResult MemoriaPersonalizada(int? tiempo)
+        {
+            if (tiempo == null)
+            {
+                tiempo = 60;
+            }
+
+            string fecha = DateTime.Now.ToLongDateString() + " -- " + DateTime.Now.ToLongTimeString();
+
+            if (this.memoryCache.Get("FECHA") == null)
+            {
+                MemoryCacheEntryOptions options = new();
+                options.SetAbsoluteExpiration(TimeSpan.FromSeconds(tiempo.Value));
+
+                this.memoryCache.Set("FECHA", fecha, options);
+                ViewData["MENSAJE"] = "Almacenado en cache";
+                ViewData["MENSAJE"] = this.memoryCache.Get("FECHA");
+            }
+            else
+            {
+                fecha = this.memoryCache.Get<string>("FECHA");
+                ViewData["MENSAJE"] = "Recuperando cache";
+                ViewData["FECHA"] = fecha;
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 5, Location = ResponseCacheLocation.Client)]
+        public IActionResult MemoriaDistribuida()
+        {
+            string fecha = DateTime.Now.ToLongDateString() + " -- " + DateTime.Now.ToLongTimeString();
+            ViewData["FECHA"] = fecha;
+            return View();
+        }
+    }
+}
--- dotnet/MvcCoreUtilidades/MvcCoreUtilidades/Program.cs
+++ dotnet/MvcCoreUtilidades/MvcCoreUtilidades/Program.cs
@@ -2,6 +2,10 @@ using MvcCoreUtilidades.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddResponseCaching();
+builder.Services.AddMemoryCache();
+builder.Services.AddDistributedMemoryCache();
+
 // Add services to the container.
 builder.Services.AddSingleton<HelperPathProvider>();
 builder.Services.AddTransient<HelperUploadFile>();
@@ -24,6 +28,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseResponseCaching();
 
 app.MapControllerRoute(
     name: "default",
