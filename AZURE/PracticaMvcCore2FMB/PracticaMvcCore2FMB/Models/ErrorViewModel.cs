9eb93a99490e1c6cde43cfd76774c89c4b4d1440
host4ideas <72515482+host4ideas@users.noreply.github.com>
2023-03-28 12:29:08 +0200

    examen movido

--- AZURE/PracticaMvcCore2FMB/PracticaMvcCore2FMB/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PracticaMvcCore2FMB.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ dotnet/PracticaMvcCore2FMB/PracticaMvcCore2FMB/ViewComponents/MenuGenerosViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticaMvcCore2FMB.Models;
+using PracticaMvcCore2FMB.Repositories;
+
+namespace PracticaMvcCore2FMB.ViewComponents
+{
+    public class MenuGenerosViewComponent : ViewComponent
+    {
+        private RepositoryLibros repositoryLibros;
+
+        public MenuGenerosViewComponent(RepositoryLibros repositoryLibros)
+        {
+            this.repositoryLibros = repositoryLibros;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Genero> generos = await this.repositoryLibros.AllGeneros();
+            return View(generos);
+        }
+    }
+}
