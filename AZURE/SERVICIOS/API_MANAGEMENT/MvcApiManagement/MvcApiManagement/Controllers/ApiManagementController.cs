bd189df8c7f070ece492b4a1cb37b08a5b199aa0
host4ideas <72515482+host4ideas@users.noreply.github.com>
2023-04-19 09:50:38 +0200

    reorganization

---
+++ AZURE/SERVICIOS/API_MANAGEMENT/MvcApiManagement/MvcApiManagement/Controllers/ApiManagementController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiManagement.Models;
+using MvcApiManagement.Services;
+
+namespace MvcApiManagement.Controllers
+{
+    public class ApiManagementController : Controller
+    {
+        private readonly ServiceApiManagement serviceApiManagement;
+
+        public ApiManagementController(ServiceApiManagement serviceApiManagement)
+        {
+            this.serviceApiManagement = serviceApiManagement;
+        }
+
+        public async Task<IActionResult> Empleados()
+        {
+            List<Empleado> data = await this.serviceApiManagement.GetEmpleadosAsync();
+            return View(data);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Departamentos()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Departamentos(string suscripcion)
+        {
+            List<Departamento> depts = await this.serviceApiManagement.GetDepartamentosAsync(suscripcion);
+            return View(depts);
+        }
+    }
+}
