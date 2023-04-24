53836c5074cff7d58c6142309c2acaeff0d9547d
host4ideas <72515482+host4ideas@users.noreply.github.com>
2023-04-24 14:24:02 +0200

    mvc despliegue cicd

---
+++ AZURE/SERVICIOS/CICD/MvcCoreDespliegue/MvcCoreDespliegue/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreDespliegue.Models;
+using System.Diagnostics;
+
+namespace MvcCoreDespliegue.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["SALUDO"] = "Bienvenido a los lunes!!!";
+            Random random = new();
+            List<int> numeros = new();
+            for (int i = 1; i <= 10; i++)
+            {
+                numeros.Add(random.Next(1, 100));
+            }
+            return View(numeros);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AZURE/SERVICIOS/CICD/MvcCoreDespliegue/MvcCoreDespliegue/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcCoreDespliegue.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
