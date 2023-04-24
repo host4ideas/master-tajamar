38f918352d106d47d28dacb1b2a4b2bcc73f5d19
host4ideas <felixmb99@hotmail.es>
2023-04-24 11:33:45 +0200

    docker

---
+++ AZURE/DOCKER/MvcDockerComics/MvcDockerComics/Controllers/ComicsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcDockerComics.Models;
+using MvcDockerComics.Repositories;
+
+namespace MvcDockerComics.Controllers
+{
+    public class ComicsController : Controller
+    {
+        private RepositoryComics repositoryComics;
+
+        public ComicsController(RepositoryComics repositoryComics)
+        {
+            this.repositoryComics = repositoryComics;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Comic> comics = await this.repositoryComics.GetComicsAsync();
+            return View(comics);
+        }
+
+        public async Task<IActionResult> Details(int idComic)
+        {
+            return View(await this.repositoryComics.FindComicAsync(idComic));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Comic comic)
+        {
+            await this.repositoryComics.InsertComicAsync(comic.Nombre, comic.Imagen);
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int idComic)
+        {
+            await this.repositoryComics.DeleteComicAsync(idComic);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AZURE/DOCKER/MvcDockerComics/MvcDockerComics/Repositories/RepositoryComics.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcDockerComics.Data;
+using MvcDockerComics.Models;
+
+namespace MvcDockerComics.Repositories
+{
+    public class RepositoryComics
+    {
+        private ComicsContext context;
+
+        public RepositoryComics(ComicsContext context)
+        {
+            this.context = context;
+        }
+
+        public Task<List<Comic>> GetComicsAsync()
+        {
+            return this.context.Comics.ToListAsync();
+        }
+
+        public Task<Comic?> FindComicAsync(int idComic)
+        {
+            return this.context.Comics.FirstOrDefaultAsync(x => x.IdComic == idComic);
+        }
+
+        public async Task<int> GetMaxComicAsync()
+        {
+            return await this.context.Comics.MaxAsync(x => x.IdComic) + 1;
+        }
+
+        public async Task InsertComicAsync(string nombre, string imagen)
+        {
+            await this.context.Comics.AddAsync(new Comic()
+            {
+                IdComic = await this.GetMaxComicAsync(),
+                Imagen = imagen,
+                Nombre = nombre,
+            });
+
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task DeleteComicAsync(int idComic)
+        {
+            Comic? comic = await this.FindComicAsync(idComic);
+            if (comic != null)
+            {
+                this.context.Comics.Remove(comic);
+                await this.context.SaveChangesAsync();
+            }
+        }
+    }
+}
