42738b68a77f1c73f93a60d93a0b5047b1b6df98
host4ideas <felixmb99@hotmail.es>
2023-06-03 15:30:49 +0200

    mvc translate

---
+++ AWS/AWS TRANSLATE/MvcAWSTranslate/MvcAWSTranslate/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcAWSTranslate.Models;
+using MvcAWSTranslate.Services;
+using System.Diagnostics;
+
+namespace MvcAWSTranslate.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ServiceTextModeration textModeration;
+
+        public HomeController(ILogger<HomeController> logger, ServiceTextModeration textModeration)
+        {
+            _logger = logger;
+            this.textModeration = textModeration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string text)
+        {
+            if (!string.IsNullOrEmpty(text))
+            {
+                ViewData["MODERATED_TEXT"] = await this.textModeration.TranslatingTextAsync(text);
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AWS/AWS TRANSLATE/MvcAWSTranslate/MvcAWSTranslate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcAWSTranslate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
